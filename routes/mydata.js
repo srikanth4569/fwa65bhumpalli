bc796af0db5244cec5ff3388fa78ec9a6759d12e
srikanth4569 <srikanthreddy439@gmail.com>
2021-10-25 22:55:06 -0500

    changes made in mydata  and users

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('mydata', { title: 'Srikanth Reddy Bhumpalli' });
+  
 });
 
 module.exports = router;
